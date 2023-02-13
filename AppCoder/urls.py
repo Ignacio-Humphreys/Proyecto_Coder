3d2378183d257abf253283f48c6cdd5043565ec2
Ignacio-Humphreys <attshumphreys@gmail.com>
2023-02-13 20:58:57 -0300

    Formularios generados + forms.py

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -3,8 +3,10 @@ from AppCoder import views
 
 urlpatterns = [
     path("", views.inicio, name="Inicio"),
-    path("Cursos", views.cursos, name="Cursos"),
-    path("Profesores", views.profesores, name="Profesores"),
-    path("Estudiantes", views.estudiantes, name="Estudiantes"),
-    path("Entregables", views.entregables, name="Entregables"),
+    path("Cursos/", views.cursos, name="Cursos"),
+    path("Profesores/", views.profesores, name="Profesores"),
+    path("Estudiantes/", views.estudiantes, name="Estudiantes"),
+    path("Entregables/", views.entregables, name="Entregables"),
+    path("cursoFormulario/", views.cursoFormulario, name="cursoFormulario"),
+    path("profesorFormulario/", views.profesorFormulario, name="profesorFormulario"),
 ]
--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from AppCoder.models import *
+from AppCoder.forms import *
 
 # Create your views here.
 def inicio(request):
@@ -17,3 +18,42 @@ def entregables(request):
 
 def estudiantes(request):
     return render(request, "../templates/AppCoder/estudiantes.html")
+
+"""def cursoFormulario(request):
+    if request.method == "POST":
+        curso = Curso(request.POST["curso"], request.POST["camada"])
+        curso.save()
+        return render(request, "../templates/AppCoder/inicio.html")
+    return render(request, "../templates/AppCoder/cursoFormulario.html")"""
+
+def cursoFormulario(request):
+    if request.method == "POST":
+        miFormulario = CursoFormulario(request.POST)
+        print(miFormulario) #Formulario vacío
+
+        if miFormulario.is_valid: #Chequea si es válido
+            informacion = miFormulario.cleaned_data
+            curso = Curso(nombre=informacion["curso"], comision=informacion["camada"])
+            curso.save() #Guarda en base de datos
+            return render(request, "../templates/AppCoder/inicio.html")
+    
+    else:
+        miFormulario = CursoFormulario()
+    
+    return render(request, "../templates/AppCoder/cursoFormulario.html", {"miFormulario":miFormulario})
+
+def profesorFormulario(request):
+    if request.method == "POST":
+        miFormulario = ProfesorFormulario(request.POST)
+        print(miFormulario) #Formulario vacío
+
+        if miFormulario.is_valid: #Chequea si es válido
+            informacion = miFormulario.cleaned_data
+            curso = Profesor(nombre=informacion["nombre"], apellido=informacion["apellido"], email=informacion["email"], profesion=informacion["profesion"])
+            curso.save() #Guarda en base de datos
+            return render(request, "../templates/AppCoder/inicio.html")
+    
+    else:
+        miFormulario = ProfesorFormulario()
+    
+    return render(request, "../templates/AppCoder/profesorFormulario.html", {"miFormulario":miFormulario})
