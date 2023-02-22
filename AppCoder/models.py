e68d196d90e7073f6951e2b823e1ac8565789e3c
Ignacio-Humphreys <attshumphreys@gmail.com>
2023-02-22 19:24:16 -0300

    Inicio de Crud

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -4,19 +4,28 @@ from django.db import models #Viene x default y ya trae el constructor dentro, p
 class Curso(models.Model): #Importo el modelo del módulo models de django
     nombre = models.CharField(max_length=40) #Indica que va a ser un campo de texto
     comision = models.IntegerField() #Indica que va a ser números enteros
+    def __str__(self):
+        return f"Nombre: {self.nombre} - Comisión: {self.comision}"
 
 class Estudiante(models.Model):
     nombre = models.CharField(max_length=30)
     apellido = models.CharField(max_length=30)
     email = models.EmailField()
+    def __str__(self):
+        return f"Nombre: {self.nombre} - Apellido: {self.apellido} - Email: {self.email}"
 
 class Profesor(models.Model):
     nombre = models.CharField(max_length=30)
     apellido = models.CharField(max_length=30)
     email = models.EmailField()
     profesion = models.CharField(max_length=30)
+    def __str__(self):
+        return f"Nombre: {self.nombre} - Apellido: {self.apellido} - Email: {self.email} - Profesión: {self.profesion}"
 
 class Entregable(models.Model):
     nombre = models.CharField(max_length=30)
     fechaDeEntrega = models.DateField()
     entregado = models.BooleanField() #Porque solo busco un True o False
+    def __str__(self):
+        return f"Nombre: {self.nombre} - Fecha de entrega: {self.fechaDeEntrega} - Entregado?: {self.entregado}"
+
--- AppCoder/views.py
+++ AppCoder/views.py
@@ -2,6 +2,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from AppCoder.models import *
 from AppCoder.forms import *
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.views.generic.edit import UpdateView
+from django.views.generic.edit import DeleteView
+from django.urls import reverse_lazy
+
 
 # Create your views here.
 def inicio(request):
@@ -49,8 +56,8 @@ def profesorFormulario(request):
 
         if miFormulario.is_valid: #Chequea si es válido
             informacion = miFormulario.cleaned_data
-            curso = Profesor(nombre=informacion["nombre"], apellido=informacion["apellido"], email=informacion["email"], profesion=informacion["profesion"])
-            curso.save() #Guarda en base de datos
+            profesor = Profesor(nombre=informacion["nombre"], apellido=informacion["apellido"], email=informacion["email"], profesion=informacion["profesion"])
+            profesor.save() #Guarda en base de datos
             return render(request, "../templates/AppCoder/inicio.html")
     
     else:
@@ -72,3 +79,60 @@ def buscar(request):
         respuesta = "Datos no enviados"
         
     return HttpResponse(respuesta)
+
+#CRUD
+def leerProfesores(request):
+    profesores = Profesor.objects.all() #Traigo a los profesores
+    contexto = {"profesores":profesores}
+    return render(request, "../templates/AppCoder/leerProfesores.html", contexto)
+
+def borrarProfesores(request, profesor_nombre):
+    profesor = Profesor.objects.get(nombre = profesor_nombre)
+    profesor.delete()
+
+    #Lee nuevamente a los profesores. Podría hacer leerProfesores()
+    profesores = Profesor.objects.all()
+    contexto = {"profesores":profesores}
+    return render(request, "../templates/AppCoder/leerProfesores.html", contexto)
+
+def editarProfesores(request, profesor_nombre): #Modifica pero termina creando un nuevo profesor
+
+    profesor = Profesor.objects.get(nombre = profesor_nombre)
+
+    if request.method == "POST":
+        miFormulario = ProfesorFormulario(request.POST)
+        print(miFormulario) #Formulario vacío
+
+        if miFormulario.is_valid: #Chequea si es válido
+            informacion = miFormulario.cleaned_data
+            profesor = Profesor(nombre=informacion["nombre"], apellido=informacion["apellido"], email=informacion["email"], profesion=informacion["profesion"])
+            profesor.save() #Guarda en base de datos
+            return render(request, "../templates/AppCoder/inicio.html")
+    
+    else:
+        miFormulario = ProfesorFormulario(initial={"nombre":profesor.nombre, "apellido":profesor.apellido, "email":profesor.email, "profesion":profesor.profesion})
+    
+    return render(request, "../templates/AppCoder/editarProfesores.html", {"miFormulario":miFormulario, "profesor_nombre":profesor_nombre})
+
+class CursoList(ListView): #Clases basadas en vistas
+    model = Curso
+    template_name = "templates/AppCoder/cursos_list.html"
+
+class CursoDetalle(DetailView): #Detalle de cada curso
+    model = Curso
+    template_name = "templates/AppCoder/curso_detalle.html"
+
+class CursoCreacion(CreateView): #Creo cursos
+    model = Curso
+    success_url = "templates/AppCoder/curso/list.html"
+    fields = ["nombre", "camada"]
+
+class CursoUpdate(UpdateView): #Actualizo cursos
+    model = Curso
+    success_url = "templates/AppCoder/curso/list.html"
+    fields = ["nombre", "camada"]
+
+class CursoDelete(DeleteView): #Borro cursos
+    model = Curso
+    success_url = "templates/AppCoder/curso/list.html"
+    
